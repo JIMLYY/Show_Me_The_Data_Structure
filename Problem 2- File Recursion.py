02e1b7a54fa56f12205f16550ca0ca710b1af37c
jian <jianwang400@gmail.com>
2019-06-01 21:14:07 -0500

    Add: add solution and related files for problem 2

---
+++ Problem 2- File Recursion.py
@@ -0,0 +1,31 @@
+import os
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    files= []
+    for file in os.listdir(path):
+        fullPath = os.path.join(path,file)
+        if os.path.isfile(fullPath) and fullPath.endswith(suffix):
+            files.append(fullPath)
+        elif os.path.isdir(fullPath):
+            files = files + find_files(suffix,fullPath)
+    return files
+
+
+print(find_files('.c','./testdir'))
+
+#print(os.listdir("./testdir"))
+#print(os.path.join("./testdir","subdir3"))
